bfdfb5016eacd65d0132b94118e6b5bc679c8956
Rundll86 <2028673870@qq.com>
2025-05-05 14:20:42 +0800

    feat: 添加 vite-plugin-zip-pack 插件以支持打包功能，并更新 .gitignore 文件以包含 archive 目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import zipPack from "vite-plugin-zip-pack";
+import packageJson from "./package.json";
 export default defineConfig({
     plugins: [
-        vue()
+        vue(),
+        zipPack({
+            outFileName: `${packageJson.name}-v${packageJson.version}.zip`,
+            inDir: "dist",
+            outDir: "archive"
+        })
     ],
     resolve: {
         alias: {
