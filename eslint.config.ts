da3b271f40341e1c38144ca9065136b05ce3ae14
Rundll86 <2028673870@qq.com>
2025-05-05 14:08:18 +0800

    改了缩进，弱智ai生成不出来commitmsg

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,28 +1,20 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-  {
-    rules: {
-      "vue/no-mutating-props": "off",
-      "vue/multi-word-component-names": "off",
-      "vue/no-use-v-if-with-v-for": "off",
-      "@typescript-eslint/no-namespace": "off"
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
+    globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+    pluginVue.configs['flat/essential'],
+    vueTsConfigs.recommended,
+    {
+        rules: {
+            "vue/no-mutating-props": "off",
+            "vue/multi-word-component-names": "off",
+            "vue/no-use-v-if-with-v-for": "off",
+            "@typescript-eslint/no-namespace": "off"
+        }
     }
-  }
-)
+);
